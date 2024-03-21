5ffeec61c301103735b582e2a720a74fec34632f
Adeleke <adelekeabiodun1996@gmail.com>
2024-03-21 20:50:20 +0100

    hide .env

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import dotenv from 'dotenv';
 import App from './App';
 import './index.css';
+
+
 
 const rootElement = document.getElementById('root');
 ReactDOM.createRoot(rootElement).render(<App />);
