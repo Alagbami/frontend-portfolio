a819f0604ab69ef2c4cd726789dba4cface454a7
lektech <adelekeabiodun1996@gmail.com>
2025-06-29 20:13:07 +0100

    latest feture

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,12 +1,16 @@
+// craco.config.js (place at your project root)
 const path = require('path');
 
 module.exports = {
-  // other webpack configurations...
-  resolve: {
-    fallback: {
-      "path": require.resolve("path-browserify"),
-      "os": require.resolve("os-browserify/browser"),
-      "crypto": require.resolve("crypto-browserify")
-    }
-  }
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback,
+        path:   require.resolve('path-browserify'),
+        os:     require.resolve('os-browserify/browser'),
+        crypto: require.resolve('crypto-browserify'),
+      };
+      return webpackConfig;
+    },
+  },
 };
